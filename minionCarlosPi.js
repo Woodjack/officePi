593c3bac304505847fe57d89ce8d0c545a217099
Justin Woodjac <jwoodjack@gmail.com>
2014-07-02 15:58:13 -0700

    point to beagleServer

--- minionCarlosPi.js
+++ minionCarlosPi.js
@@ -8,7 +8,7 @@ console.log(' connecting: http://10.0.1.220:7000');
 //  Create a socket.io client object and connect to overlord ip  
 //
 var clientio  = require('socket.io-client');
-var client    = clientio.connect('http://localhost:7000');
+var client    = clientio.connect('http://10.0.1.220:7000');
 
 //  Emits message to overlord to checkin with comp name
 //
