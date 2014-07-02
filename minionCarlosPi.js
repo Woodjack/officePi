d8d034a0f343c3178d199b1b54e2739a47e249d3
Justin Woodjac <jwoodjack@gmail.com>
2014-07-02 15:56:59 -0700

    cleanup

--- minionCarlosPi.js
+++ minionCarlosPi.js
@@ -10,18 +10,20 @@ console.log(' connecting: http://10.0.1.220:7000');
 var clientio  = require('socket.io-client');
 var client    = clientio.connect('http://localhost:7000');
 
+//  Emits message to overlord to checkin with comp name
+//
+client.emit('checkIn', 'minionCarlos-Pi');
+
+
+//********************
+//  MESSAGE HANDLERS
+
 //  On message updateLED, change LEd to the color in the data json object
 //
 client.on('updateLED', function(data) {
   console.log('  -- chaging LED to: ', data);
+  
+  //  ********
+  // Carlos will add function to make the LED change to the color
+  //
 });
-
-
-
-//  Emits message to overlord to checkin with comp name
-//
-client.emit('checkIn', 'minionC-Pi');
-
-//  notifies you of sucess
-console.log(' -- checked into overlord ')
-console.log(' ')
--- overlord.js
+++ overlord.js
@@ -9,9 +9,13 @@ console.log(' listening: http://localhost:7000');
 var io = require('socket.io').listen(7000);
  
 io.sockets.on('connection', function (socket) {
-  
+ 
+//  When a socket emit is recieved, then run this function
+//
   socket.on('checkIn', function(data) {
     console.log('  -- User checked in: ' + data);
+    // Sends message to the connected client
+    //
     socket.emit('updateLED', {color:'red'});
 
   });
