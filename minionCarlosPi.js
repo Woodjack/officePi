2b99f889fc7f9c41a6d1a589334eb68688e9d285
Carlos Hernandez <carlosomarhe@gmail.com>
2014-07-03 14:56:50 -0700

    Update minionCarlosPi.js

--- minionCarlosPi.js
+++ minionCarlosPi.js
@@ -1,7 +1,9 @@
 var piblaster = require("pi-blaster.js");
 
 // Set first pin at 40%
-piblaster.setPwm(22, 0.4);
+piblaster.setPwm(22, 1);
 
 // Set second pin at 100%
-piblaster.setPwm(4, 1);
+piblaster.setPwm(17, 1);
+
+piblaster.setPwm(4, 0);
