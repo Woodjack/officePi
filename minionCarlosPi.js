b2401b8616a2e4462440645681082fcfa9369cb7
Carlos Hernandez <carlosomarhe@gmail.com>
2014-07-03 16:02:30 -0700

    Update minionCarlosPi.js

--- minionCarlosPi.js
+++ minionCarlosPi.js
@@ -1,9 +1,38 @@
+
+console.log(' ------------------------ ');
+console.log(' ------- officePI ------- ');
+console.log(' ** minion ** ');
+console.log(' connecting: http://10.0.1.220:7000');
+
+
+var clientio = require('socket.io-client');
+var client = clientio.connect('http://localhost:7000');
+
+client.emit('checkIn', 'clientName');
+
+
+client.on('message', function(data) {
+  console.log('message', data);
+});
+
+
+client.on('updateLED', function(data) {
 var piblaster = require("pi-blaster.js");
-
-// Set first pin at 40%
-piblaster.setPwm(22, 0);
-
-// Set second pin at 100%
-piblaster.setPwm(17, 1);
-
-piblaster.setPwm(4, 1);
+    
+  console.log('  -- chaging LED to: ', data);
+  if( data == "red" ){
+    piblaster.setPwm(22, 1);
+    piblaster.setPwm(17, 1);
+    piblaster.setPwm(4, 0);
+  };
+  else if( data == "green" ){
+    piblaster.setPwm(22, 0);
+    piblaster.setPwm(17, 1);
+    piblaster.setPwm(4, 1);
+  };
+  else{
+    piblaster.setPwm(22, 1);
+    piblaster.setPwm(17, 0);
+    piblaster.setPwm(4, 1);    
+  };
+};
