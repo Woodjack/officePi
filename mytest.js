8cbfa1609cfa0b896c6511e6985fe2c93457df29
Carlos Hernandez <carlosomarhe@gmail.com>
2014-07-07 12:36:05 -0700

    Update mytest.js

--- mytest.js
+++ mytest.js
@@ -1,7 +1,7 @@
 //it can run without connection with master
 console.log('***JUST FOR FUN***');
 var piblaster = require("pi-blaster.js");
-var a = 0;
+var a = 9;
 
 //  console.log("turn off");
 //    piblaster.setPwm(22, 1);
