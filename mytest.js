d9bc9f13c2b58dd5a9957323efa38a896dec82c7
CarlosOhh <carlosomarhe@gmail.com>
2014-07-07 19:38:31 +0000

    Merge branch 'master' of https://github.com/justinwoodjack/officePi

--- mytest.js
+++ mytest.js
@@ -1,28 +1,26 @@
 //it can run without connection with master
 console.log('***JUST FOR FUN***');
 var piblaster = require("pi-blaster.js");
+var a = 1;
 
-setTimeout(function () {
-setTimeout(function () {
-setTimeout(function () {
-  debugger;
-  console.log("turn off");
-    piblaster.setPwm(22, 1);
-    piblaster.setPwm(17, 1);
-    piblaster.setPwm(4, 1);
-}, 3000);
-  debugger;
-  console.log("blue");
-    piblaster.setPwm(22, 1);
-    piblaster.setPwm(17, 0);
-    piblaster.setPwm(4, 1);
-}, 3000);
+//  console.log("turn off");
+//    piblaster.setPwm(22, 1);
+//    piblaster.setPwm(17, 1);
+//    piblaster.setPwm(4, 1);
+
+//  console.log("blue");
+//    piblaster.setPwm(22, 1);
+//    piblaster.setPwm(17, 0);
+//    piblaster.setPwm(4, 1);
+if (a===1){
   console.log("green");
     piblaster.setPwm(22, 0);
     piblaster.setPwm(17, 1);
     piblaster.setPwm(4, 1);
-}, 3000);
-console.log("red");
-piblaster.setPwm(22, 1);
-piblaster.setPwm(17, 1);
-piblaster.setPwm(4, 0);
+}
+else {
+  console.log("red");
+    piblaster.setPwm(22, 1);
+    piblaster.setPwm(17, 1);
+    piblaster.setPwm(4, 0);
+}
