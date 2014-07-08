e7c6693db40f2b9f12db6177752c2d20f7f3a329
Carlos Hernandez <carlosomarhe@gmail.com>
2014-07-08 14:23:03 -0700

    Update minionCarlosPi.js

--- minionCarlosPi.js
+++ minionCarlosPi.js
@@ -74,6 +74,7 @@ client.on('updateLED', function(data) {
     }
      console.log(count);
      piblaster.setPwm(17, count); 
+     piblaster.setPwm(4, 1-count); 
     }, 1);
   } else {
     console.log('I do not know this color');
