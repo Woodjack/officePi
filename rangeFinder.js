0b2c3d4f102a8307e45501a45647ac5744d521c9
Justin Woodjac <jwoodjack@gmail.com>
2015-01-20 22:08:58 -0800

    Range Finder ready for testing

--- rangeFinder.js
+++ rangeFinder.js
@@ -3,6 +3,15 @@
 //
 //  Calculates the range using ultrasonic range finder
 //
+var gpio = require("pi-gpio");
+
+gpio.open(16, "output", function(err) {        // Open pin 16 for output
+  console.log('pin 16 ready for output')
+});
+
+gpio.open(18, "input", function(err) {        // Open pin 16 for output
+  console.log('pin 18 ready for input')
+});
 
 
 // sleep function
@@ -20,18 +29,6 @@ function sleep(milliseconds) {
 
 
 
-var gpio = require("pi-gpio");
-
-gpio.open(16, "output", function(err) {        // Open pin 16 for output
-  console.log('pin 16 ready for output')
-});
-
-gpio.open(18, "input", function(err) {        // Open pin 16 for output
-  console.log('pin 18 ready for input')
-});
-
-
-
 gpio.read(16, function(err, value) {
   if(err) throw err;
   console.log(value);    // The current state of the pin
@@ -41,36 +38,42 @@ var pulse = function(){
 
   gpio.write(16, 1, function(){
 
-    setTimeout( pulse(),
-    500)
   })
 
-  # Send 10us pulse to trigger
-  GPIO.output(GPIO_TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER, False)
-  start = time.time()
-  while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
+  time.sleep(1)
 
-  while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
+  gpio.write(16, 1, function(){
 
-  # Calculate pulse length
-  elapsed = stop-start
+  })
 
-  # Distance pulse travelled in that time is time
-  # multiplied by the speed of sound (cm/s)
-  distance = elapsed * 34000
+  var starTime = Date.now();
+  var stopTime;
 
-  # That was the distance there and back so halve the value
-  distance = distance / 2
+  while(true){
+    gpio.read(16, function(err, value) {
+      if(err) throw err;
+      starTime = Date.now();
+      if(value == 1) break;
+    });
+  }
 
-  print "Distance : %.1f" % distance
+  while(true){
+    gpio.read(16, function(err, value){
+      if(err) throw err;
+      stopTime = Date.now();
+      if(value == 0) break;
+    })
+  }
 
+  var elapsed = stopTime-startTime;
+
+  // Distance pulse travelled in that time is time
+  // multiplied by the speed of sound (cm/s)
+  var distance = elapsed *34000;
+  distance = distance/2;
+
+  console.log("Distance :  " + distance)
 }
-
-
 
 
 gpio.write(16, 0, function(){
@@ -78,34 +81,3 @@ gpio.write(16, 0, function(){
   setTimeout( pulse(),
     500)
 })
-
-
-
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
-
-# Allow module to settle
-time.sleep(0.5)
-
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-while GPIO.input(GPIO_ECHO)==0:
-start = time.time()
-
-while GPIO.input(GPIO_ECHO)==1:
-stop = time.time()
-
-# Calculate pulse length
-elapsed = stop-start
-
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34000
-
-# That was the distance there and back so halve the value
-distance = distance / 2
-
-print "Distance : %.1f" % distance
